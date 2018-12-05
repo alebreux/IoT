98f32c05d7c81bf84504cc9c9d4087e43b1b167f
Noé Domenech de Cellès <noe.domenech-de-celles@mines-albi.fr>
2018-12-05 11:50:25 +0100

    MAJ des méthodes ouvrir volet et fermer volet

--- hello.py
+++ hello.py
@@ -7,6 +7,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    return "Hello World !"
+
+
+@app.route('/lumiere/allume/<valeurlum>')
+def lumiereAllume(valeurlum):
+    return 'True %s' % valeurlum
+
+
+@app.route('/lumiere/eteint/<valeurlum>')
+def lumiereEteint(valeurlum):
+    return 'False %s' % valeurlum
+
+
+@app.route('/volet/ouvre/<valeurVolet>')
+def voletOuvre(valeurVolet):
     GPIO.setmode(GPIO.BOARD)
     Motor1A = 16
     Motor1B = 18
@@ -26,21 +41,28 @@ def hello():
     GPIO.output(Motor1E,GPIO.LOW)
 
     GPIO.cleanup()
-
-    return "Test"
-
-@app.route('/lumiere/allume/<valeurlum>')
-def lumiereAllume(valeurlum):
-    return 'True %s' % valeurlum
-
-@app.route('/lumiere/eteint/<valeurlum>')
-def lumiereEteint(valeurlum):
-    return 'False %s' % valeurlum
-
-@app.route('/volet/ouvre/<valeurVolet>')
-def voletOuvre(valeurVolet):
     return 'True %s' % valeurVolet
+
 
 @app.route('/volet/ferme/<valeurVolet>')
 def voletFerme(valeurVolet):
+    GPIO.setmode(GPIO.BOARD)
+    Motor1A = 16
+    Motor1B = 18
+    Motor1E = 22
+
+    GPIO.setup(Motor1A,GPIO.OUT)
+    GPIO.setup(Motor1B,GPIO.OUT)
+    GPIO.setup(Motor1E,GPIO.OUT)
+    print("Turning motor on")
+
+    GPIO.output(Motor1A,GPIO.LOW)
+    GPIO.output(Motor1B,GPIO.HIGH)
+    GPIO.output(Motor1E,GPIO.HIGH)
+
+    sleep(20)
+    print("stopping motor")
+    GPIO.output(Motor1E,GPIO.LOW)
+
+    GPIO.cleanup()
     return 'False %s' % valeurVolet
